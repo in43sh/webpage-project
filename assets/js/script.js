4c9935a7cd722a73b95493f9461b75355c09dbcd
Sergey <in43sh@gmail.com>
2020-11-13 16:42:18 -0700

    Switch content when clicking on menu items.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,4 @@
-document.getElementById("linkIntro").addEventListener("click", displayIntro);
-document.getElementById("linkHistory").addEventListener("click", displayHistory);
-document.getElementById("linkImpact").addEventListener("click", displayImpact);
-document.getElementById("linkJobs").addEventListener("click", displayJobs);
-document.getElementById("linkEthics").addEventListener("click", displayEthics);
-
-let displayHistory = () => {
-    let current = document.getElementById("intro");
-    current.style.display = "none";
-    let toDisplay = document.getElementById("history");
-    toDisplay.style.display = "block";
-}
+$('a').on('click', function(){
+    var target = $(this).attr('rel');
+    $("#"+target).show().siblings("article").hide();
+});
